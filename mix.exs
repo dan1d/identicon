ec9879960a45bfcb5c753a7ad38082a56e0668a8
Daniel Dominguez <danielfromarg@gmail.com>
2018-06-24 19:56:35 -0300

    Add docs.

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Identicon.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:egd, github: "erlang/egd"}
+      {:egd, github: "erlang/egd"},
+      {:ex_doc, "~> 0.11", only: :dev}
     ]
   end
 
